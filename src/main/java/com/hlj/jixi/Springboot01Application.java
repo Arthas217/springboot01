79cabc580d449f619464264b137d6961d60e93a7
zuocheng <442854399@qq.com>
2020-12-03 17:50:48 +0800

    1

--- src/main/java/com/hlj/jixi/Springboot01Application.java
+++ src/main/java/com/hlj/jixi/Springboot01Application.java
@@ -1,8 +1,5 @@
 package com.hlj.jixi;
 
-import com.hlj.jixi.control.HelloWorld;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
